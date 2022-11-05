16ee74036cdbc8c7b1c78ff0dbdc61ca36253726
Caleb_Du <Caleb_Du@zju.edu.cn>
2022-11-05 17:17:29 +0800

    check boundary in cuda reduce

--- src/ndarray_backend_cuda.cu
+++ src/ndarray_backend_cuda.cu
@@ -509,8 +509,11 @@ void Matmul(const CudaArray &a, const CudaArray &b, CudaArray *out, uint32_t M,
 ////////////////////////////////////////////////////////////////////////////////
 
 __global__ void ReduceMaxKernel(const scalar_t *a, scalar_t *out,
-                                size_t reduce_size) {
+                                size_t reduce_size, size_t len) {
     size_t gid = threadIdx.x + blockDim.x * blockIdx.x;
+    if (gid >= len) {
+        return;
+    }
     scalar_t maxValue = a[gid * reduce_size];
     for (size_t i = gid * reduce_size; i < (gid + 1) * reduce_size; i++) {
         maxValue = max(maxValue, a[i]);
@@ -531,13 +534,17 @@ void ReduceMax(const CudaArray &a, CudaArray *out, size_t reduce_size) {
      */
     /// BEGIN YOUR SOLUTION
     auto dim = CudaOneDim(out->size);
-    ReduceMaxKernel<<<dim.grid, dim.block>>>(a.ptr, out->ptr, reduce_size);
+    ReduceMaxKernel<<<dim.grid, dim.block>>>(a.ptr, out->ptr, reduce_size,
+                                             out->size);
     /// END YOUR SOLUTION
 }
 __global__ void ReduceSumKernel(const scalar_t *a, scalar_t *out,
-                                size_t reduce_size) {
+                                size_t reduce_size, size_t len) {
     size_t gid = threadIdx.x + blockDim.x * blockIdx.x;
     scalar_t sum = 0.0f;
+    if (gid >= len) {
+        return;
+    }
     for (size_t i = gid * reduce_size; i < (gid + 1) * reduce_size; i++) {
         sum += a[i];
     }
@@ -556,7 +563,8 @@ void ReduceSum(const CudaArray &a, CudaArray *out, size_t reduce_size) {
      */
     /// BEGIN YOUR SOLUTION
     auto dim = CudaOneDim(out->size);
-    ReduceSumKernel<<<dim.grid, dim.block>>>(a.ptr, out->ptr, reduce_size);
+    ReduceSumKernel<<<dim.grid, dim.block>>>(a.ptr, out->ptr, reduce_size,
+                                             out->size);
     /// END YOUR SOLUTION
 }
 
